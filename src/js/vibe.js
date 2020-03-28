544f9435b31b6781cfbcb6b094df1f05bd320866
Akramwired <kahd.unplugged@gmail.com>
2020-03-28 13:06:28 +0600

    greetings changed

--- src/js/vibe.js
+++ src/js/vibe.js
@@ -2,7 +2,7 @@ const vibes = [
     "...and you are awesome!",
     "...have a wonderful day!",
     "...and you've got this!",
-    "...and so is this puppy!"
+    "...and things will get better!"
 ];
 
 var vibe = vibes[Math.floor(Math.random() * Math.floor(vibes.length))];
